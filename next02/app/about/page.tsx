df380454c907ac5e47f6629f42a6fc4fec29bd93
Devansh Behl <heydevanshbehl@gmail.com>
2023-10-16 22:09:37 +0530

    nextjs tutorial

---
+++ next02/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function About() {
+  // throw new Error('Error in About page')
+  return (
+    <>
+    <h1>
+      <h1>Link</h1>
+      <Link href="/">Link to home page</Link>
+    </h1>
+    </>
+  )
+}
